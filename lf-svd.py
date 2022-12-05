c781f09035dd97109f3443f308c5a4e40a089d45
Phil Bladen <philjb@vt.edu>
2022-12-05 15:02:50 -0500

    add a method to make this file callable as external module

--- lf-svd.py
+++ lf-svd.py
@@ -1,6 +1,13 @@
 import scipy.sparse as sc
 import numpy as np
 
+def create_prob_matrix(input_matrix_file: str, alpha: list, k: int, output_matrix_file: str):
+    sp_matrix = sc.load_npz(input_matrix_file).todense()
+    A = createA(sp_matrix, alpha)
+    nA = createK(A, k)
+    print("\n", nA)
+    np.save(output_matrix_file, nA)
+
 def createA(Y, alpha):
     n = Y.shape[0]
     m = Y.shape[1]
@@ -10,15 +17,15 @@ def createA(Y, alpha):
     sum_all = np.sum(Y)
 
     for i in range(n):
+        if i % 100 == 0: print("LFSVD first loop i %d of %d" % (i, n))
         for j in range(m):
             v = [Y[i,j], 1/n*sums_j[0,j], 1/m*sums_i[i,0], 1/(n*m)*sum_all]
             A[i][j] = np.dot(v, alpha)
-            #print(A)
     return A
-
 
 def createK(A, k):
     # U, S, V = svd(A)
+    print("starting step 2 of LFSVD")
     U, S, V = np.linalg.svd(A, full_matrices=False)
     counter = 0
     for eig in S:
@@ -27,14 +34,5 @@ def createK(A, k):
         counter += 1   
     E = np.diag(S)
     nA = U @ E @ V
+    print("finished step 2 of LFSVD")
     return nA
-
-#Load here the matrix with whatever name we provide 
-sp_matrix = sc.load_npz("parsed_graph.npz").todense()
-print(sp_matrix)
-alpha = [0, 1, 0, 0]
-#M = [[1, 0, 0, 0, 1, 0],[0, 1, 1, 0, 0, 0],[0, 0, 1, 1, 1, 1]]
-
-A = createA(sp_matrix,alpha)
-nA = createK(A, 1)
-print("\n", nA)
